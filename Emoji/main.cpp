9dd92ae32328117349f00080831c35e427f96d58
Eduard <terriblegoodday@icloud.com>
2019-10-26 09:19:31 +1000

    Initial Commit

---
+++ Emoji/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Emoji
+//
+//  Created by Eduard Dzhumagaliev on 10/26/19.
+//  Copyright © 2019 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
